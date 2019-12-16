127f202ad060d21e17b1de778d79a37971f3cb14
Brian Mark Anderson <bmanderson@mdanderson.org>
2019-12-16 04:17:48 +0100

    broke up normalize and Threshold_Images, added param tags

--- Generators.py
+++ Generators.py
@@ -28,14 +28,29 @@ class Image_Processor(object):
 
 
 class Normalize_Images(Image_Processor):
-    def __init__(self, mean_val=0, std_val=1, lower_bound=-np.inf, upper_bound=np.inf):
-        self.mean_val, self.std_val, self.lower, self.upper = mean_val, std_val, lower_bound, upper_bound
+    def __init__(self, mean_val=0, std_val=1):
+        '''
+        :param mean_val: Mean value to normalize to
+        :param std_val: Standard deviation value to normalize to
+        '''
+        self.mean_val, self.std_val = mean_val, std_val
 
     def preload_single_image_process(self, image, annotation):
         image = (image - self.mean_val)/self.std_val
+        return image, annotation
+
+class Threshold_Images(Image_Processor):
+    def __init__(self, lower_bound=-np.inf, upper_bound=np.inf):
+        '''
+        :param lower_bound: Lower bound to threshold images, normally -3.55 if Normalize_Images is used previously
+        :param upper_bound: Upper bound to threshold images, normally 3.55 if Normalize_Images is used previously
+        '''
+        self.lower = lower_bound
+        self.upper = upper_bound
+
+    def post_load_process(self, images, annotations):
         image[image<self.lower] = self.lower
         image[image>self.upper] = self.upper
-        return image, annotation
 
 
 class Perturbation_Class(Image_Processor):
@@ -188,7 +203,7 @@ class Rotate_Images_2D_Processor(Image_Processor):
         '''
         :param image_size: size of image row/col
         :param by_patient: perform on all images in stack, or vary for each one
-        :param variation: range of values np.round(np.arange(start=0, stop=2.6, step=0.5),2)
+        :param variation: range of values np.round(np.arange(start=-5, stop=5, step=1))
         '''
         self.image_size = image_size
         self.variation = variation
