697b9f3fe4a444d3f407b7a370f0c34eea8083f3
brianmanderson <markba122@gmail.com>
2024-10-30 12:07:15 -0400

    roll back to using parse for these things

--- TFRecord_to_Dataset_Generator.py
+++ TFRecord_to_Dataset_Generator.py
@@ -89,16 +89,16 @@ class DataGeneratorClass(object):
             for image_processor in image_processors:
                 print(image_processor)
                 if type(image_processor) not in [dict, set]:
-                    processor = image_processor.__call__
+                    processor = image_processor.parse
                     if debug:
                         if data is None:
                             data = next(iter(self.data_set))
                         if type(data) is tuple:
-                            data = image_processor.__call__(*data)
+                            data = image_processor.parse(*data)
                         elif data is not None:
-                            data = image_processor.__call__(data)
+                            data = image_processor.parse(data)
                     else:
-                        processor = tf.function(image_processor.__call__)
+                        processor = tf.function(image_processor.parse)
                     self.data_set = self.data_set.map(processor, num_parallel_calls=self.in_parallel)
                 elif type(image_processor) in [dict, set]:
                     data = None
