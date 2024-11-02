94f18e66526895e0c569c80cc9fa841996a8f75f
brianmanderson <markba122@gmail.com>
2024-11-01 20:29:07 -0400

    updates

--- TFRecord_to_Dataset_Generator.py
+++ TFRecord_to_Dataset_Generator.py
@@ -90,6 +90,7 @@ class DataGeneratorClass(object):
         data = None
         if debug and data is None:
             data = next(iter(self.data_set))
+        is_tuple = False
         if image_processors is not None:
             for image_processor in image_processors:
                 print(image_processor)
@@ -98,19 +99,26 @@ class DataGeneratorClass(object):
                     if debug:
                         if data is None:
                             data = next(iter(self.data_set))
-                        if type(data) is tuple:
-                            data = image_processor.parse(*data)
-                        elif data is not None:
+                        if data is not None:
                             data = image_processor.parse(data)
                     else:
                         processor = tf.function(image_processor.parse)
-                    self.data_set = self.data_set.map(processor, num_parallel_calls=self.in_parallel)
+                    if True:
+                        self.data_set = self.data_set.map(
+                            lambda *features: processor(*features) if isinstance(features[0], dict) else
+                            (processor(*features) if len(features) > 1 else processor(features[0])),
+                            num_parallel_calls=self.in_parallel)
+                    elif not is_tuple:
+                        self.data_set = self.data_set.map(lambda features: processor(features), num_parallel_calls=self.in_parallel)
+                    else:
+                        self.data_set = self.data_set.map(lambda features: processor(*features), num_parallel_calls=self.in_parallel)
                 elif type(image_processor) in [dict, set]:
                     data = None
                     value = None
                     if type(image_processor) is dict:
                         value = [image_processor[i] for i in image_processor][0]
                     if 'batch' in image_processor:
+                        is_tuple = True
                         assert value is not None, "You need to provide a batch size with {'batch':batch_size}"
                         self.total_examples = self.total_examples//value
                         self.data_set = self.data_set.batch(value, drop_remainder=False)
