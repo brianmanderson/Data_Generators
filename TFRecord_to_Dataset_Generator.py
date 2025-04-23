56bd60c0dd172d56d02d814dcacf6a6eb7612b13
brianmanderson <markba122@gmail.com>
2025-04-23 14:51:23 -0400

    update submodules

--- TFRecord_to_Dataset_Generator.py
+++ TFRecord_to_Dataset_Generator.py
@@ -83,13 +83,19 @@ class DataGeneratorClass(object):
                 self.total_examples += int(examples)
             else:
                 self.total_examples += 1
-        parsed_image_dataset = raw_dataset.map(tf.function(return_parse_function(features),),
-                                               num_parallel_calls=self.in_parallel, synchronous=self.synchronus)
+        if self.synchronus is None:
+            parsed_image_dataset = raw_dataset.map(tf.function(return_parse_function(features),),
+                                                num_parallel_calls=self.in_parallel)
+        else:
+            parsed_image_dataset = raw_dataset.map(tf.function(return_parse_function(features),), synchronous=self.synchronus)
         Decode = DecodeImagesAnnotations(d_type_dict=d_types)
         if debug:
             data = next(iter(parsed_image_dataset))
             data = Decode.parse(image_features=data)
-        self.data_set = parsed_image_dataset.map(tf.function(Decode.parse), num_parallel_calls=self.in_parallel, synchronous=self.synchronus)
+        if self.synchronus is None:
+            self.data_set = parsed_image_dataset.map(tf.function(Decode.parse), num_parallel_calls=self.in_parallel)
+        else:
+            self.data_set = parsed_image_dataset.map(tf.function(Decode.parse), synchronous=self.synchronus)
 
     def compile_data_set(self, image_processors=None, debug=False):
         data = None
@@ -108,7 +114,10 @@ class DataGeneratorClass(object):
                             data = image_processor.parse(data)
                     else:
                         processor = tf.function(image_processor.parse)
-                    self.data_set = self.data_set.map(processor, num_parallel_calls=self.in_parallel, synchronous=self.synchronus)
+                    if self.synchronus is None:
+                        self.data_set = self.data_set.map(processor, num_parallel_calls=self.in_parallel)
+                    else:
+                        self.data_set = self.data_set.map(processor, synchronous=self.synchronus)
                     # if True:
                     #     self.data_set = self.data_set.map(
                     #         lambda *features: processor(*features) if isinstance(features[0], dict) else
